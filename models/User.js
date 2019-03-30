bf270bbc1868272cb2193cc265d389f60d5da95c
muzzyay <muzzyay@gmail.com>
2019-03-30 13:01:22 -0700

    default pic changed

--- models/User.js
+++ models/User.js
@@ -11,8 +11,7 @@ var UserSchema = new Schema({
   },
   image: {
     type: String,
-    default:
-      "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png"
+    default: "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png"
   },
   email: {
     type: String,
