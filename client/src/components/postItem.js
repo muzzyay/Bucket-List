d920d95852d65830bb42eefff83d2f81476802a1
muzzyay <muzzyay@gmail.com>
2019-03-31 10:37:43 -0700

    enter key issue fixed

--- client/src/components/postItem.js
+++ client/src/components/postItem.js
@@ -16,36 +16,45 @@ class postItem extends React.Component {
         });
     };
 
-    
+    preventEnterKey = e => {
+        if(e.which===13){
+            e.preventDefault();
+            return false;
+        }
+
+    }
+
+
     render() {
         return <>
-        <div className="modal" id="postItemBtn" tabindex="-1" role="dialog">
-            <div className="modal-dialog" role="document">
-                <div className="modal-content">
-                    <div className="modal-body">
-                        <h4 className="text-center"><i class="far fa-plus-square"></i> Post an Item</h4>
-                        <hr />
-                        <form>
-                            <div class="form-group">
-                                <label for="name">Title</label>
-                                <input
-                                    value={this.state.title}
-                                    name="title"
-                                    onChange={this.handleInputChange}
-                                    type="text" class="form-control" id="title" aria-describedby="title" placeholder="Enter a Bucketlist Item" />
-                            </div>
+            <div className="modal" id="postItemBtn" tabindex="-1" role="dialog">
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                        <div className="modal-body">
+                            <h4 className="text-center"><i class="far fa-plus-square"></i> Post an Item</h4>
+                            <hr />
+                            <form onKeyPress={this.preventEnterKey}>
+                                <div class="form-group">
+                                    <label for="name">Title</label>
+                                    <input
+                                        value={this.state.title}
+                                        name="title"
+                                        onChange={this.handleInputChange}
+                                        type="text" class="form-control" id="title" aria-describedby="title" placeholder="Enter a Bucketlist Item" />
+                                </div>
 
-                            
-                        </form>
 
-                        <button onClick={()=>{this.props.handleFormSubmit(this.state.title);
-                        this.setState({title: ""});
-                        }} data-dismiss="modal" class="btn btn-primary col-12"><i class="far fa-plus-square"></i> Post</button>
+                            </form>
 
+                            <button onClick={() => {
+                                this.props.handleFormSubmit(this.state.title);
+                                this.setState({ title: "" });
+                            }} data-dismiss="modal" class="btn btn-primary col-12"><i class="far fa-plus-square"></i> Post</button>
+
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div></>
+            </div></>
 
 
     }
